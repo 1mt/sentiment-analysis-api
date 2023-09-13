3cfda6e0e69b4815ca2d0993ec5c75076dfa0aff
Matt <mt__@outlook.com>
2023-09-13 21:18:15 +0100

    Add sklearn functionality

--- api/index.py
+++ api/index.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Load dataset and split into train and test sets
+boston = datasets.load_boston()
+X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, test_size=0.2, random_state=42)
 
-@app.route('/about')
-def about():
-    return 'About'
+# Train a linear regression model
+model = LinearRegression().fit(X_train, y_train)
+
+@app.route('/')
+def index():
+    return "Welcome to the House Price Prediction API! Use /predict endpoint to get price predictions."
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.json
+        prediction = model.predict(np.array([data['features']]))
+        return jsonify({'prediction': prediction[0]})
+    except Exception as e:
+        return jsonify({'error': str(e)})
